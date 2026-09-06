55ebab34a85efa6e806b0d847afb177e4818aad2
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-09-07 06:27:46 +0700

    feat: remove commented-out accounts and sessions table definitions from schema

--- db/schema.ts
+++ db/schema.ts
@@ -41,45 +41,6 @@ export const credentialsRelations = relations(credentialsTable, ({ one }) => ({
   }),
 }));
 
-// export type ProviderType = "GITHUB" | "DISCORD" | "GOOGLE";
-
-// export const accountsTable = sqliteTable(
-//   "accounts",
-//   {
-//     id: text("id")
-//       .$defaultFn(() => nanoid())
-//       .notNull()
-//       .unique(),
-//     userId: text("user_id").notNull(),
-//     provider: text("provider", {
-//       enum: ["GITHUB", "DISCORD", "GOOGLE"],
-//     }).notNull(),
-//     providerAccountId: text("provider_account").notNull(),
-//     profile: text("profile", { mode: "json" }),
-//     accessToken: text("access_token"),
-//     refreshToken: text("refresh_token"),
-//   },
-//   // I add composite key so that each user can have only one provider type.
-//   (table) => {
-//     return {
-//       id: primaryKey({ columns: [table.userId, table.provider] }),
-//     };
-//   },
-// );
-
-// export const accountsRelations = relations(accountsTable, ({ one }) => ({
-//   user: one(usersTable, {
-//     fields: [accountsTable.userId],
-//     references: [usersTable.id],
-//   }),
-// }));
-
-// export const sessionsTable = sqliteTable("sessions", {
-//   sid: text("sid").primaryKey(),
-//   expired: integer("expired"),
-//   sess: text("sess", { mode: "json" }),
-// });
-
 // For constructing user-data object to pass around
 type UTI = typeof usersTable.$inferInsert;
 type CTI = typeof credentialsTable.$inferInsert;
